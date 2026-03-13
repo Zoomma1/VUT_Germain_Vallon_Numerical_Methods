6a92a2c61e2b30c0ef3ec230e40a9e2829855e3e
Zoomma1 <victorgerm1@hotmail.fr>
2026-03-13 12:29:26 +0100

    Added build

---
+++ src/build_exe.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Enable virtual environment
+call .venv\Scripts\activate
+
+REM Clear previous builds
+rmdir /s /q build
+rmdir /s /q dist
+del /q main.spec
+
+REM Build the executable with PyInstaller and include the CSV file
+pyinstaller --onefile --windowed --add-data "IVP_problems.csv;." --name "VUT IVP Solver Comparison" main.py
+
+REM Copy the CSV file next to the .exe in the dist folder
+copy IVP_problems.csv dist\
+
+echo.
+echo Build completed. The executable and CSV file are located in the 'dist' folder.
+pause
